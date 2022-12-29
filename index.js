46338c50d8762c38d94e856dd2ee6158b8df9892
Terry Sahaidak <tasssik@gmail.com>
2022-12-30 00:05:02 +0200

    use branch from pr

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ import {
   info,
   error as coreError,
 } from "@actions/core";
-import { context } from "@actions/github";
+import { context, getOctokit } from "@actions/github";
 import axios from "axios";
+
+const octokit = getOctokit(process.env.GITHUB_TOKEN);
 
 startGroup("Preparing CircleCI Pipeline Trigger");
 const repoOrg = context.repo.owner;
@@ -16,17 +18,18 @@ info(`Org: ${repoOrg}`);
 info(`Repo: ${repoName}`);
 const ref = context.ref;
 
-const getBranch = () => {
-  if (ref.startsWith("refs/heads/")) {
-    return ref.substring(11);
-  } else if (ref.startsWith("refs/pull/")) {
-    info(`This is a PR. Using head PR branch`);
-    const pullRequestNumber = ref.match(/refs\/pull\/([0-9]*)\//)[1];
-    const newref = `pull/${pullRequestNumber}/head`;
-    return newref;
-  }
-  return ref;
-};
+
+// const getBranch = () => {
+//   if (ref.startsWith("refs/heads/")) {
+//     return ref.substring(11);
+//   } else if (ref.startsWith("refs/pull/")) {
+//     info(`This is a PR. Using head PR branch`);
+//     const pullRequestNumber = ref.match(/refs\/pull\/([0-9]*)\//)[1];
+//     const newref = `pull/${pullRequestNumber}/head`;
+//     return newref;
+//   }
+//   return ref;
+// };
 const getTag = () => {
   if (ref.startsWith("refs/tags/")) {
     return ref.substring(10);
@@ -45,46 +48,55 @@ const parameters = {
   GHA_Event: context.eventName,
 };
 
-const metaData = getInput("GHA_Meta");
-if (metaData.length > 0) {
-  Object.assign(parameters, { GHA_Meta: metaData });
-}
+async function main() {
+  let metaData = getInput("GHA_Meta");
 
-const body = {
-  parameters: parameters,
-};
+  metaData = metaData.replace('/testflight', '');
 
-const tag = getTag();
-const branch = getBranch();
+  if (metaData.length > 0) {
+    Object.assign(parameters, { GHA_Meta: metaData });
+  }
 
-if (tag) {
-  Object.assign(body, { tag });
-} else {
+  const body = {
+    parameters: parameters,
+  };
+
+  const pr = await octokit.rest.pulls.get(context.issue);
+
+  const tag = getTag();
+  const branch = pr.data.head.ref // getBranch();
   Object.assign(body, { branch });
+
+  // if (tag) {
+  //   Object.assign(body, { tag });
+  // } else {
+  // }
+
+  const url = `https://circleci.com/api/v2/project/gh/${repoOrg}/${repoName}/pipeline`;
+
+  info(`Triggering CircleCI Pipeline for ${repoOrg}/${repoName}`);
+  info(`Triggering URL: ${url}`);
+  if (tag) {
+    info(`Triggering tag: ${tag}`);
+  } else {
+    info(`Triggering branch: ${branch}`);
+  }
+  info(`Parameters:\n${JSON.stringify(parameters)}`);
+  endGroup();
+
+  axios
+    .post(url, body, { headers: headers })
+    .then((response) => {
+      startGroup("Successfully triggered CircleCI Pipeline");
+      info(`CircleCI API Response: ${JSON.stringify(response.data)}`);
+      endGroup();
+    })
+    .catch((error) => {
+      startGroup("Failed to trigger CircleCI Pipeline");
+      coreError(error);
+      setFailed(error.message);
+      endGroup();
+    });
 }
 
-const url = `https://circleci.com/api/v2/project/gh/${repoOrg}/${repoName}/pipeline`;
-
-info(`Triggering CircleCI Pipeline for ${repoOrg}/${repoName}`);
-info(`Triggering URL: ${url}`);
-if (tag) {
-  info(`Triggering tag: ${tag}`);
-} else {
-  info(`Triggering branch: ${branch}`);
-}
-info(`Parameters:\n${JSON.stringify(parameters)}`);
-endGroup();
-
-axios
-  .post(url, body, { headers: headers })
-  .then((response) => {
-    startGroup("Successfully triggered CircleCI Pipeline");
-    info(`CircleCI API Response: ${JSON.stringify(response.data)}`);
-    endGroup();
-  })
-  .catch((error) => {
-    startGroup("Failed to trigger CircleCI Pipeline");
-    coreError(error);
-    setFailed(error.message);
-    endGroup();
-  });
+main();
